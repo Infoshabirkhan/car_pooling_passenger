f769b8e34ddc83b8bd2b8d8fee37832a2ad2f436
jmmabdulmalik <jmm.abdulmalik@gmail.com>
2022-05-30 07:03:45 -0700

    color

--- lib/Model/utils/appcolors.dart
+++ lib/Model/utils/appcolors.dart
@@ -20,7 +20,7 @@ class AppColors{
   static Color kRedThird= Color(0xffDE1717);
   static Color bluelight= Color(0xffBAD2E3);
   static Color kCallIcon= Color(0xff38B80C);
-  static Color bluelight = Color(0xffBAD2E3);
+
 
 
 
