83927c9597338cbc46cb87d24d974f1aa9b3648a
saif-flutter <syedsaif4all@gmail.com>
2022-06-01 17:15:04 +0500

    Dialog Boxes in Arrived

--- lib/View/bottom_navigaion_views/request_accepted_view/rider_arrived_card.dart
+++ lib/View/bottom_navigaion_views/request_accepted_view/rider_arrived_card.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
+import '../../../Model/utils/appcolors.dart';
+import '../../../Model/utils/appicons.dart';
+
 
 class RiderArrivedCard extends StatelessWidget {
   final VoidCallback onTap;
@@ -8,16 +11,162 @@ class RiderArrivedCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: onTap,
-      child: Positioned(
-        left: 15.w,
-        right: 15.w,
-        bottom: 38.h,
+    return Positioned(
+      left: 15.w,
+      right: 15.w,
+      bottom: 38.h,
+      child: InkWell(
+        onTap: onTap,
         child: Container(
-          color: Colors.white,
+          color: AppColors.kWhite,
           width: 200.sp,
           height: 200.sp,
+
+          child: Column(
+           children: [
+             Expanded(
+                 flex: 2,
+                 child: Row(
+                   children: [
+                     Expanded(child: Icon(
+                       AppIcons.account_person, color: const Color(0xff57A3DE),
+                       size: 52.sp,)),
+
+                     Expanded(
+                         flex: 3,
+                         child: Column(
+                           crossAxisAlignment: CrossAxisAlignment.start,
+                           children: [
+                             Expanded(child: Row(
+                               children: [
+                                 Expanded(child: Text('John Doe',
+                                   style: TextStyle(fontSize: 15.sp,
+                                       fontWeight: FontWeight.bold),)),
+
+                                 Expanded(child: Padding(
+                                   padding:  EdgeInsets.only(top: 23.sp),
+                                   child: Column(
+                                     children: [
+                                       Expanded(
+                                         flex: 2,
+                                         child: Align(
+                                           alignment: Alignment.center,
+                                           child: Icon(AppIcons.phone_call,color: AppColors.kCallIcon,size: 32.sp,)),),
+
+                                       Expanded(
+                                           child: Text(
+                                             'Make Call',
+                                             style: TextStyle(
+                                               fontFamily: 'SF Pro Text',
+                                               fontSize: 12.sp,
+                                               color:  AppColors.kCallIcon,
+                                             ),
+                                           )
+                                       )
+
+                                     ],
+                                   ),
+                                 ),),
+
+                               ],
+                             )),
+
+                           ],
+                         )),
+
+                   ],
+                 )),
+             Expanded(
+               child: Padding(
+                 padding:  EdgeInsets.only(left:36.sp),
+                 child: Column(
+                   children: [
+                     Expanded(
+                       child: Row(
+                         children: [
+                           Expanded(
+                               child: Text(
+                                 'Car Model:',
+                                 style: TextStyle(
+                                   fontFamily: 'SF Pro Text',
+                                   fontSize: 12.sp,
+                                   color: AppColors.kGreySecondary,
+                                 ),
+                               )),
+                           Expanded(
+                             flex: 2,
+                               child: Text(
+                                 textAlign: TextAlign.left,
+                                 'Plate No:',
+                                 style: TextStyle(
+                                   fontFamily: 'SF Pro Text',
+                                   fontSize: 12.sp,
+                                   color: AppColors.kGreySecondary,
+                                 ),
+                               )),
+                         ],
+                       ),
+                     ),
+                     Expanded(
+                       child: Row(
+                         children: [
+                           Expanded(
+                               child: Text(
+                                 'Alto VXR',
+                                 style: TextStyle(
+                                   fontFamily: 'SF Pro Text',
+                                   fontSize: 15.sp,
+                                   color: AppColors.kblackSecondary,
+                                 ),
+                               )),
+                           Expanded(
+                             flex: 2,
+                               child: Text(
+                                 textAlign: TextAlign.left,
+                                 'DH-233R',
+                                 style: TextStyle(
+                                   fontFamily: 'SF Pro Text',
+                                   fontSize: 15.sp,
+                                   color: AppColors.kblackSecondary,
+                                 ),
+                               )),
+                         ],
+                       ),
+                     ),
+                   ],
+                 ),
+               ),
+             ),
+             Expanded(
+                 flex: 2,
+                 child: Row(
+
+                   children: [
+                     Expanded(
+                         flex: 2,
+                         child: Container(
+                       // color: Colors.red,
+                         alignment: Alignment.centerRight,
+                         child: Icon(AppIcons.car,color: AppColors.kBlue,))),
+                     Expanded(
+                        flex: 5,
+                         child: Container(
+                           margin: EdgeInsets.only(left: 15.sp),
+                       alignment: Alignment.centerLeft,
+                       child: Text(
+                         'Your Rider has Arrived',
+                         style: TextStyle(
+                           fontFamily: 'SF Pro Text',
+                           fontSize: 15.sp,
+                           color: AppColors.kBlack,
+                         ),
+                       ),
+                     ))
+                   ],
+                 ))
+
+           ],
+          ),
         ),
       ),
     );
--- lib/View/bottom_navigaion_views/request_accepted_view/toward_direction_card.dart
+++ lib/View/bottom_navigaion_views/request_accepted_view/toward_direction_card.dart
@@ -1,5 +1,7 @@
+import 'package:car_pooling_passanger/Model/utils/appcolors.dart';
+import 'package:car_pooling_passanger/Model/utils/appicons.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class TowardDirectionCard extends StatelessWidget {
   final VoidCallback onTap;
@@ -8,13 +10,117 @@ class TowardDirectionCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-        onTap: onTap,
-        child: Container(
+    return Positioned(
+      bottom: 0,
+      right: 0,
+      left: 0,
+      child: InkWell(
+          onTap: onTap,
+          child: Container(
+            height: 140.h,
+             color: Colors.white,
+            child: Column(
+              children: [
+                Expanded(
+                    child: Container(
+                  alignment: Alignment.centerLeft,
+                  padding: EdgeInsets.only(left: 30.sp, top: 12.sp),
+                   // color: Colors.blue,
+                  child: Column(
+                    children: [
+                      Expanded(
+                          child: Text(
+                        'Distance',
+                        style: TextStyle(
+                          fontFamily: 'SF Pro Text',
+                          fontSize: 12.sp,
+                          color: AppColors.kGreySecondary,
+                        ),
+                      )),
+                      Expanded(
+                          child: Text(
+                        '2.6 km',
+                        style: TextStyle(
+                          fontFamily: 'SF Pro Text',
+                          fontSize: 15.sp,
+                          color: AppColors.kblackSecondary,
+                        ),
+                      ))
+                    ],
+                  ),
+                )),
+                Expanded(
+                    flex: 2,
+                    child: Container(
+                      padding: EdgeInsets.only(left: 30.sp, top: 13.sp),
+                      // color: Colors.yellow,
+                      child: Row(
+                        children: [
+                          Expanded(
+                            child: Container(
+                              alignment: Alignment.centerLeft,
+                              // color: Colors.white,
+                              child: Column(
+                                children: [
+                                  Expanded(child: Container(
+                                    alignment: Alignment.bottomLeft,
+                           //         color: Colors.blue,
+                                    child: Text(
+                                      'Estimated time',
+                                      style: TextStyle(
+                                        fontFamily: 'SF Pro Text',
+                                        fontSize: 12.sp,
+                                        color: AppColors.kGreySecondary,
+                                      ),
 
-          width: 200,
-          height: 200,
-          color: Colors.red,
-        ));
+                                    ),
+                                  )),
+                                  Expanded(child: Container(
+                                    alignment: Alignment.topLeft,
+                                    child: Text(
+                                      '1hr 23min',
+                                      style: TextStyle(
+                                        fontFamily: 'SF Pro Text',
+                                        fontSize: 15.sp,
+                                        color: AppColors.kblackSecondary,
+                                      ),
+                                    ),
+                                  )),
+
+
+                                ],
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            child: Container(
+                              // color: Colors.red,
+                              child: Column(
+                                children: [
+                                  Expanded(child: Image.asset('assets/images/customer_care.png')),
+                                  Expanded(
+
+                                      child: Text(
+                                    'Emergency Call',
+                                    style: TextStyle(
+                                      fontFamily: 'SF Pro Text',
+                                      fontSize: 12.sp,
+                                      color: AppColors.kRedSecondary,
+                                    ),
+                                  )),
+
+                                ],
+                              ),
+                            ),
+                          ),
+
+                        ],
+                      )
+                      ),
+                    ),
+              ],
+            ),
+          )),
+    );
   }
 }
